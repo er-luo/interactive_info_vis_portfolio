40908bf153d0a7a8395758b44fc1a11c8035b347
er-luo <er.caluo@gmail.com>
2026-02-06 22:19:13 -0800

    fixing formatting for correct deployment

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -1,12 +1,12 @@
 // Instance-mode sketch for tab 2
 registerSketch('sk2', function (p) {
-function setup() {
+p.setup = function () {
   p.createCanvas(p.windowWidth, p.windowHeight);
-  angleMode(DEGREES);
-}
+  p.angleMode(DEGREES);
+};
 
-function draw() {
-  background('black');
+p.draw = function (){
+  p.background('black');
 
   let hr = hour() % 12;
   let mn = minute();
@@ -26,48 +26,48 @@ function draw() {
   let monthAbv = months[m];
   
 
-  translate(width / 2, height / 2);
+  p.translate(width / 2, height / 2);
 
-  noFill();
-  strokeCap(ROUND);
+  p.noFill();
+  p.strokeCap(ROUND);
   
   //faded circles in background to help visualize progress
-  stroke('#4a1c1c');
-  strokeWeight(12);
-  circle(0,0,300);
+  p.stroke('#4a1c1c');
+  p.strokeWeight(12);
+  p.circle(0,0,300);
   
-  stroke('#163b16');
-  strokeWeight(12);
-  circle(0,0,260);
+  p.stroke('#163b16');
+  p.strokeWeight(12);
+  p.circle(0,0,260);
   
-  stroke('#2c2c42');
-  strokeWeight(12);
-  circle(0,0,220);
+  p.stroke('#2c2c42');
+  p.strokeWeight(12);
+  p.circle(0,0,220);
 
   // Seconds ring
-  stroke('#ff6464');
-  strokeWeight(12);
-  arc(0, 0, 300, 300, -90, hrAngle - 90);
+  p.stroke('#ff6464');
+  p.strokeWeight(12);
+  p.arc(0, 0, 300, 300, -90, hrAngle - 90);
 
   // Minutes ring
-  stroke('#00ff64');
-  strokeWeight(12);
-  arc(0, 0, 260, 260, -90, mnAngle - 90);
+  p.stroke('#00ff64');
+  p.strokeWeight(12);
+  p.arc(0, 0, 260, 260, -90, mnAngle - 90);
 
   // Hours ring
-  stroke('#6464ff');
-  strokeWeight(12);
-  arc(0, 0, 220, 220, -90, scAngle - 90);
+  p.stroke('#6464ff');
+  p.strokeWeight(12);
+  p.arc(0, 0, 220, 220, -90, scAngle - 90);
   
   
 
   // Digital time + date
-  noStroke();
-  fill(0);
-  textAlign(CENTER, CENTER);
-  textSize(24);
-  fill('white');
-  text(
+  p.noStroke();
+  p.fill(0);
+  p.textAlign(CENTER, CENTER);
+  p.textSize(24);
+  p.fill('white');
+  p.text(
     nf(hour(), 2) + ":" + nf(mn, 2) + ":" + nf(sc, 2) + 
     '\n' + dayOfWeek + ', ' + monthAbv + ". " + m,
     0,
@@ -75,6 +75,8 @@ function draw() {
   );
 
 
-}
+};
+
+p.windowResized = function () { p.resizeCanvas(p.windowWidth, p.windowHeight); };
 
 });
